3781f21e8294af85364f00e2c3a47ad3e46262de
Jamesllllllllll <james@jameskeezer.dev>
2025-02-18 22:32:25 -0500

    Passing gosec tests

--- json.go
+++ json.go
@@ -27,5 +27,9 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	w.Write(dat)
+	_, writeErr := w.Write(dat)
+	if writeErr != nil {
+		log.Printf("error writing header")
+		return
+	}
 }
